25b200974d8b60a2da39747d5ba361254dfbd6fa
Peter Kim <kroits09@gmail.com>
2018-04-29 18:28:50 +0900

    Add necessary modules for the project

---
+++ scraper.js
@@ -0,0 +1,14 @@
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+
+// Connect to The API URL (http://shirts4mike.com/shirts.php)
+
+// Scraping Data
+  // Read the Data
+
+  // Parse the Data
+
+// Save the Data into a CSV file
+  // The CSV file must be named for the date it was created, e.g. 2016-11-21.csv.
+  // The Order of CSV file column headers are Title, Price, ImageURL, URL, and Time.
+  // The CSV file should be saved inside the ‘data’ folder.
