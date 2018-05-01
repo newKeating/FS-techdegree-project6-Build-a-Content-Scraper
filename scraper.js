a327d08e648ffdb15775c4b7d0ec90f67a9e625c
Peter Kim <kroits09@gmail.com>
2018-05-01 12:27:11 +0900

    Add complete scrape functions

--- scraper.js
+++ scraper.js
@@ -1,12 +1,52 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 
-// Connect to The API URL (http://shirts4mike.com/shirts.php)
+// Request HTML body to The URL (http://shirts4mike.com/shirts.php)
+  // Used npm module request-promise for requesting
+  // cheerio makes jQuery available in Node.js
 
-// Scraping Data
-  // Read the Data
+const shirtsLink = [];
+const shirtsInfo = [];
 
-  // Parse the Data
+// scrapeShirtsLink() scrapes html from 'http://shirts4mike.com/shirts.php' and get links to each shirt item.
+const scrapeShirtsLink = async () => {
+  await rp('http://shirts4mike.com/shirts.php')
+    .then((html) => {
+      let $ = cheerio.load(html);
+      $('ul.products a').each((i, link) => {
+        shirtsLink[i] = `http://shirts4mike.com/${$(link).attr("href")}`;
+      });
+      console.log(shirtsLink);
+    })
+}
+
+// scrapeShirtsInfo() scrapes html from each shirt item link and get each shirt info: Title, Price, ImageURL.
+  // Also, it saves shirt-URL and the current time into shirtsInfo-array with scraped shirt-infos.
+const scrapeShirtsInfo = async () => {
+  let shirtsHtml = await Promise.all(shirtsLink.map(link => rp(link)));
+  let date = new Date().toISOString().slice(0,10);
+  let time = new Date().toString().slice(16);
+  shirtsHtml.forEach((shirtHtml, i) => {
+    let $ = cheerio.load(shirtHtml);
+    let shirtInfo = {
+      title: $('.shirt-details h1').text().substr(4),
+      price: $('.price').text(),
+      imageUrl: `http://shirts4mike.com/${$('img').attr('src')}`,
+      url: shirtsLink[i],
+      date: `${date} ${time}`
+    }
+    shirtsInfo.push(shirtInfo);
+  });
+  console.log(shirtsInfo);
+}
+
+// Because of ES2017 async and await features, it is possible to use .then() method.
+scrapeShirtsLink()
+  .then(scrapeShirtsInfo)
+  .catch((err) => {
+    console.log(err);
+  });
+
 
 // Save the Data into a CSV file
   // The CSV file must be named for the date it was created, e.g. 2016-11-21.csv.
